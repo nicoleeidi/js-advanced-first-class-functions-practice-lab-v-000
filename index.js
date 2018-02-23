aed4803fe6957f722d78e864b80e2e97692f7e45
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-23 18:41:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Code your solution in this file!
 function logDriverNames(driver){
-  driver.forEach(function(el,i,arr){ //should we not put unused arguments?
+  driver.forEach(function(el,i,arr){
     console.log(el.name)
   })
 }
 function logDriversByHometown(driver,location){
   names= driver.filter(function(person){return person.hometown== location})
-  names.forEach(function(el,i,arr)){console.log(el.name)})
+  names.forEach(function(el,i,arr){console.log(el.name)})
 }
-function driversByRevenue(driver){ //how does it know referring to revenues?
-  return driver.sort(function(driver1,driver2){
+function driversByRevenue(driver){
+  const driversCopy= [...driver]
+  return driversCopy.sort(function(driver1,driver2){
     return driver1.revenue - driver2.revenue
   })
+
 }
 function driversByName(driver){
   return driver.sort(function(a,b){
@@ -22,3 +24,6 @@ function totalRevenue(driver){
   return driver.reduce(function(agg,el,i,arr){return agg + el.revenue},0)
 }
 //or return driver.reduce(function(agg,el,i,arr){return agg.totalRevenue + el.revenue},{totalRevenue:0})
+function averageRevenue(driver){
+  return totalRevenue(driver) / driver.length
+}
